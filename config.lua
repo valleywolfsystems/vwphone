680da61920a17b50d265079462169b32f83e917d
valleywolfsystems <valleywolf.systems@gmail.com>
2024-11-24 03:54:11 -0400

    Initial commit

---
+++ config.lua
@@ -0,0 +1,37 @@
+
+
+Config = {}
+Config.OpenPhone = 'm'   --## Phone open key ## https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.Phones = {"phone"}  -- # item name
+Config.Locale = 'en'
+Config.Fahrenheit = false
+
+Config.ESXVersion = "1.2" -- 1.1 OR 1.2
+
+
+-- Discord WebHook - start --
+Config.Carseller = 'Discord Webhook'
+Config.JobNotif = 'Discord Webhook'
+Config.TwitterWeb = 'Discord Webhook'
+Config.YellowWeb = 'Discord Webhook'
+Config.InstagramWeb = 'Discord Webhook'
+Config.Crypto = 'Discord Webhook'
+Config.BankTrasnfer = 'Discord Webhook'
+Config.BankLimit = 5000 -- # Minimum money transfer for discord webhook
+-- Discord WebHook - end --
+
+
+
+-- Phone Settings - Start --
+
+Config.UseMumbleVoIP    = false -- Use Frazzle's Mumble-VoIP Resource (Recomended!) https://github.com/FrazzIe/mumble-voip
+Config.PMAVoice         = true
+Config.UseTokoVoIP      = false
+Config.SaltyChat        = false
+
+Config.CallPhone        = true  -- If the player is not in the game or there is no item on it, it will give a warning.
+
+-- Phone Settings - Finish --
+
+Config.ValePrice        = 100    -- Vale Price
+Config.TaxiPrice        = 75     -- Taxi Price ( 75$/KM )
---
+++ locales/en.lua
@@ -0,0 +1,93 @@
+Locales['en'] = {
+    ['phone_ring']				= "Your phone is ringing",
+    ['new_one_message']			= "New Message",
+    ['new_message_hotline']		= " New message to the hotline",
+    ['new_message_normal']		= " From New Message",
+    ['offline_call']		= " The person you are calling is currently unavailable.",
+    ['nophone_call']		= " The person you are calling has hung up their phone.",
+    ['blcknumber_call']		= " The other party has blocked you ",
+    ["no_item"] = "No phone",
+    ["bank_noplayer"] = "Make sure you have entered correct information",
+    ["phone_time"] = "try again later",
+	
+	-- Vale
+	["vale_get"] = "You bought your car for $ 100",
+	["vale_gete"] = "You Can Use Only One Valet Service At A Time.",
+	["vale_getr"] = "Your Vehicle Is Already Away Marked On The Map.",
+    ["vale_checmoney"] = "You owe the government more than $ 10, you can't get your car back until you've paid your fines!",
+    ["vale_fee"] = "Valet Fee",
+
+    ["car_seller"] = "Your vehicle has been towed because your vehicle has been sold.",
+    ["car_purchase"] = " Car purchase",
+    ["car_sale"] = " Car sale",
+    -- cyrpto
+    ["not_enough_money"] = "you don't have enough money",
+    ["not_enough_coin"] = "you don't have enough coin",
+    ["cyrpto_buy"] = "You bought ",
+    ["cyrpto_sell"] = "You sold ",
+    ["not_crypto"] = " you must buy first",
+    ["cyrpto_transfer"] = " Transfer done successfully",
+    ["cyrpto_transfer2"] = " Crypto Transferred to Your Account",
+    ["cyrpto_check"] = " Check the quantity",
+    ["cyrpto_notplayer"] = "Phone number not contacted",
+
+    -- JOB Notifi
+    ["not_player"] = "No active person found in the professional unit",
+    ["job_notifi"] = "New notification came to the system",
+
+    -- Race
+    ["race_distance"] = "You are away, go to the marked area",
+
+    -- Billing
+    ["bill_paid"] = "The bill has been paid $",
+    ["bill_nocash"] = "You don't have enough money.",
+
+    -- Billing
+    ["carseller_ownbuy"] = "You Can't Buy Your Own Vehicle",
+    ["carseller_nobank"] = "You Don't Have Enough Money in the Bank",
+    
+    ["carseller_soldvehicle"] = "Your vehicle has been sold",
+    ["carseller_buyvehicle"] = "You bought the vehicle. Use for good day",
+
+    -- Group Message
+    ["group_created"] = "Group created",
+    ["group_deleted"] = "The group has been deleted.",
+    ["group_newc"] = "New contact added",
+    ["group_newmes"] = " New message to the group",
+    ["group_newpeop"] = "A new person has been added to the group",
+    ["group_delpeop"] = " Left",
+
+    -- Bank
+    ["bank_yourself"] = "You cannot transfer to your self!",
+    ["bank_nomoney"] = "Not enough money to transfer!",
+    ["bank_transfer"] = " Bank transfer was made",
+    ["bank_playertransfer"] = "Bank transfer came from ",
+    ["bank_systemnophone"] = "Phone number not registered in the system. ",
+
+    -- Taxi
+    ["taxi_customer"] = "Customer found",
+    ["taxi_noemployees"] = "No employees",
+    ["taxi_nomoney"] = "You don't have enough money",
+    ["taxi_gotjob"] = " got the job",
+    ["taxi_driverway"] = "Taxi driver is on the way ",
+    ["taxi_driverpicked"] = "Taxi driver picked you up from your location",
+    ["taxi_customerleft"] = "You left the customer where he/she wanted",
+    ["taxi_earnings"] = "Taxi earnings",
+    ["taxi_payment"] = "Taxi payment",
+    ["taxi_cancel"] = "Taxi canceled",
+    ["taxi_customercar"] = "You have to be in the car to receive customers",
+    ["taxi_customernotveh"] = "The customer is not in your vehicle",
+    ["taxi_customernotveh2"] = "The customer is not in the vehicle",
+    ["taxi_notcar"] = "You are not in the car",
+    ["taxi_customerloc"] = "You left the customer at the location",
+
+    --Mail
+    ["email_received"] = " Email received from person",
+
+    --Tinder
+    ["tinder_matched"] = "You are matched",
+    ["tinder_notusername"] = "This username is used by someone else.",
+    ["tinder_sucess"] = "Your account has been created. Good matches :)",
+    ["tinder_notlogin"] = "Login failed, wrong info!",
+    ["tinder_login"] = "Signed in",
+}
